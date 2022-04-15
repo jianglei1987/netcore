b666aa7728d1de93f1b0fdeb63c1f0837e484d83
jianglei <715708689@qq.com>
2022-04-15 22:13:53 +0800

    提交最简单的helloworld

---
+++ samples/HelloWorldConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+
+Host.CreateDefaultBuilder()
+    .ConfigureWebHostDefaults(builder =>
+        builder.Configure(app =>
+        {
+            app.Run(async context =>
+             {
+                 await context.Response.WriteAsync("Hello World");
+             });
+        }))
+    .Build().Run();
