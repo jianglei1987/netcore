e1d708a5bad1db8f318d7a21940311b15b4818a7
jianglei <715708689@qq.com>
2022-04-15 22:53:32 +0800

    最简单的中间间定义

---
+++ samples/HelloWorldMiddleware/Program.cs
@@ -0,0 +1,31 @@
+﻿Func<RequestDelegate, RequestDelegate> Middleware3 = (RequestDelegate next)=>
+{
+    return next;
+};
+
+Host.CreateDefaultBuilder()
+    .ConfigureWebHostDefaults(webBuilder =>
+    {
+        webBuilder.Configure(app =>
+        {
+            app.Use(Middleware1);
+            app.Use(Middleware2);
+            app.Use(Middleware3);
+            app.Run(async (context) => { await context.Response.WriteAsync("!"); });
+        });
+    })
+    .Build().Run();
+
+static RequestDelegate Middleware1(RequestDelegate next)
+    => async context =>
+    {
+        await context.Response.WriteAsync("Hello");
+        await next(context);
+    };
+
+static RequestDelegate Middleware2(RequestDelegate next)
+    => async context =>
+    {
+        await context.Response.WriteAsync("World");
+        await next(context);
+    };
