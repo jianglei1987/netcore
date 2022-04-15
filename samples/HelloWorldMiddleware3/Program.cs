477a2770483e915a09383379b0d5c6dffbf054a9
jianglei <715708689@qq.com>
2022-04-15 23:18:29 +0800

    中间件约定定义

---
+++ samples/HelloWorldMiddleware3/Program.cs
@@ -0,0 +1,32 @@
+﻿//按照约定中间件
+Host.CreateDefaultBuilder()
+    .ConfigureWebHostDefaults(builder =>
+        builder.Configure(app =>
+        {
+            app.UseMiddleware<StringContentMiddleware>("Hello");
+            app.UseMiddleware<StringContentMiddleware>("World!", false);
+        }))
+    .Build().Run();
+
+sealed class StringContentMiddleware
+{
+    private readonly string _contents;
+
+    private readonly RequestDelegate _next;
+
+    private readonly bool _forewardToNext;
+
+    public StringContentMiddleware(RequestDelegate next, string contents, bool forewardToNext = true)
+    {
+        _contents = contents;
+        _next = next;
+        _forewardToNext = forewardToNext;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        await context.Response.WriteAsync(_contents);
+        if (_forewardToNext)
+            await _next(context);
+    }
+}
